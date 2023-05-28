ebc063fa162763dd338167781face7f510a07b2d
lisandrojm <lisandrojm@gmail.com>
2023-05-28 07:15:28 -0300

    GET/limit // status // commments

--- src/routes/products.routers.js
+++ src/routes/products.routers.js
@@ -14,21 +14,26 @@ if (!fs.existsSync(productosFilePath) || fs.readFileSync(productosFilePath, 'utf
 
 /////////////////////////////////////////////////////
 /* La ruta raíz GET /  */
-// Obtener todos los productos
+// Obtener todos los productos (Incluyendo la limitación ?limit)
 router.get('/', (req, res) => {
   try {
+    const limit = req.query.limit; // Obtener el valor del parámetro 'limit' de la consulta (si existe)
+
     // Leer el archivo JSON de productos
     const productosData = fs.readFileSync(productosFilePath, 'utf8');
     const products = JSON.parse(productosData);
-    return res.status(200).json(products);
+
+    // Obtener productos limitados según el parámetro 'limit' o todos los productos si no se especifica el parámetro
+    const limitedProducts = limit ? products.slice(0, parseInt(limit)) : products;
+
+    return res.status(200).json(limitedProducts);
   } catch (error) {
-    return res.status(500).json({ error: 'Error al obtener los productos' });
+    return res.status(500).send({ status: 'error', error: 'Error al obtener los productos' });
   }
 });
 
 /////////////////////////////////////////////////////
 /* La ruta GET /:pid /  */
-
 // Obtener producto con el id proporcionado
 router.get('/:pid', (req, res) => {
   try {
@@ -42,12 +47,12 @@ router.get('/:pid', (req, res) => {
     const product = products.find((p) => p.id === pid);
 
     if (!product) {
-      return res.status(404).json({ error: 'Producto no encontrado' });
+      return res.status(404).send({ status: 'error', error: 'Producto no encontrado' });
     }
 
     return res.status(200).json(product);
   } catch (error) {
-    return res.status(500).json({ error: 'Error al obtener el producto' });
+    return res.status(500).send({ status: 'error', error: 'Error al obtener el producto' });
   }
 });
 /////////////////////////////////////////////////////
@@ -56,6 +61,11 @@ router.get('/:pid', (req, res) => {
 router.post('/', (req, res) => {
   try {
     const { title, description, code, price, stock, category, thumbnails } = req.body;
+
+    // Verificar campos obligatorios
+    if (!title || !description || !code || !price || !stock || !category) {
+      return res.status(500).send({ status: 'error', error: 'Faltan campos obligatorios' });
+    }
 
     // Leer el archivo JSON de productos
     const productosData = fs.readFileSync(productosFilePath, 'utf8');
@@ -82,11 +92,59 @@ router.post('/', (req, res) => {
 
     // Guardar los productos actualizados en el archivo JSON
     fs.writeFileSync(productosFilePath, JSON.stringify(products, null, 2));
-
-    return res.status(201).json(newProduct);
+    return res.status(201).send({ status: 'created', message: 'Producto agregado correctamente' });
   } catch (error) {
-    return res.status(500).json({ error: 'Error al agregar el producto' });
+    return res.status(500).send({ status: 'error', error: 'Error al agregar el producto' });
   }
 });
 
 module.exports = router;
+
+/////////////////////////////////////////////////////
+/* Postman Testing */
+
+/////////////////////////////////////////////////////
+/* La ruta raíz GET /  */
+// Obtener todos los productos */
+/* http://localhost:8080/api/products/ */
+// Retorna todos los productos
+
+/////////////////////////////////////////////////////
+/* La ruta raíz GET (Incluyendo la limitación ?limit)  */
+// Obtener todos los productos */
+/* http://localhost:8080/api/products?limit=2 */
+// Retorna los dos primeros productos
+
+/////////////////////////////////////////////////////
+/* La ruta GET /:pid /  */
+// Obtener producto con el id proporcionado */
+/* http://localhost:8080/api/products/64ae */
+// Retorna el producto con el id seleccionado
+
+/////////////////////////////////////////////////////
+/* La ruta raíz POST /  */
+// Agregar un nuevo producto
+/* http://localhost:8080/api/products/ */
+// Templates de productos para el Body de Postman
+
+// Sin campo thumbnails
+/* {
+  "title": "Producto Postman",
+  "description": "Este es un producto de Postman",
+  "code": "a1",
+  "price": 100,
+  "stock": 10,
+  "category": "Categoría Postman",
+} */
+// Retorna : created : Producto agregado correctamente
+
+// Sin otro campo que no sea "thumbnails", en este caso sin el campo "precio"
+/* {
+  "title": "Producto Postman",
+  "description": "Este es un producto de Postman",
+  "code": "a1",
+  "stock": 10,
+  "category": "Categoría Postman",
+  "thumbnails": "Con imagen"
+} */
+// Retorna : error: Faltan campos obligatorios
